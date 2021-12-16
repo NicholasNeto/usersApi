165cf9d9ced611e877920361fd94dee64c7ca7ac
NicholasNeto <nicholasneto@outlook.com>
2021-12-16 00:10:15 -0300

    used try catch for response error  in TurnUserAdminUseCase

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,12 +3,16 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.params
-    const user = this.turnUserAdminUseCase.execute({user_id })
-    return response.status(200).json(user)
+    try {
+      const { user_id } = request.params
+      const user = this.turnUserAdminUseCase.execute({ user_id })
+      return response.status(200).json(user)
+    } catch (error) {
+      response.status(400).json({ error })
+    }
   }
 }
 

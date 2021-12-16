93be688024c9d886aa2a41472172aa0ac8719fba
NicholasNeto <nicholasneto@outlook.com>
2021-12-16 00:09:41 -0300

    used try catch for response error  in listAllUsersController

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,14 +6,13 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    const user_id = ( request.headers['user_id'] ) as string
-    const users = this.listAllUsersUseCase.execute( { user_id })
-
-    if (users.length < 0) {
-      return response.status(400).json('Erro, for request need valid user')
+    try {
+      const user_id = ( request.headers['user_id'] ) as string
+      const users = this.listAllUsersUseCase.execute( { user_id })
+      return response.status(200).json(users)
+    } catch (error) {
+      return response.status(400).json({ error }) 
     }
-
-    return response.status(200).json(users)
   }
 }
 

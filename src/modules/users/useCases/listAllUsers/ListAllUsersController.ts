920aa280dc384f36ae1677c7a6ab258d6c67af51
NicholasNeto <nicholasneto@outlook.com>
2021-12-15 23:07:18 -0300

    fix logic of users

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,8 +6,13 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.params
-    const users = this.listAllUsersUseCase.execute({ user_id })
+    const user_id = ( request.headers['user_id'] ) as string
+    const users = this.listAllUsersUseCase.execute( { user_id })
+
+    if (users.length < 0) {
+      return response.status(400).json('Erro, for request need valid user')
+    }
+
     return response.status(200).json(users)
   }
 }
